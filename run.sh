50754f33ab7e90809767cbf5a87a51c1d0cbf9d6
PKQ <zhutaonan@i-i.ai>
2019-11-15 18:26:34 +0800

    update

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 python demo/PMTD_demo.py \
 --image_path=/home/shizai/datadisk2/ocr_data/val/img/icdar15_img_327.jpg \
 --model_path=models/PMTD_ICDAR2017MLT.pth \
-#--output_type=Images
+--output_type=Points
