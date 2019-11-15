61f4309e08bced7a1c7334ca7cf3d93849ef050d
PKQ <zhutaonan@i-i.ai>
2019-11-15 17:20:19 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 python demo/PMTD_demo.py \
 --image_path=/home/shizai/datadisk2/ocr_data/val/img/icdar15_img_327.jpg \
---model_path=models/PMTD_ICDAR2017MLT.pth
+--model_path=models/PMTD_ICDAR2017MLT.pth \
+--output_type=Points
