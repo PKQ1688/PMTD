06aad405427f3aa04b524361ab22aa6dd671964d
PKQ <zhutaonan@i-i.ai>
2019-11-19 13:40:44 +0800

    update

--- tmp/miao_test.py
+++ tmp/miao_test.py
@@ -2,3 +2,10 @@
 # @author :adolf
 import os
 
+data_test = '/home/shizai/datadisk4/SynthText/miaodata'
+
+data_list = os.listdir(data_test)
+
+data_list = [data_name for data_name in data_list if 'png' in data_name]
+
+print(data_list)
