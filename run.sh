ffb923bc579351b937d8b4633dc6fa0b36a219d8
PKQ <zhutaonan@i-i.ai>
2019-12-02 15:55:04 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=6 \
 python demo/PMTD_demo.py \
 --image_path=/home/shizai/adolf/ocr/ocr_project_use/shenzhou_project/301_1329_.png \
 --model_path=models/PMTD_ICDAR2017MLT.pth \
 --output_type=Image #Image,Points
+#--image_path=/home/shizai/adolf/ocr/ocr_project_use/shenzhou_project/data/001.jpg \
